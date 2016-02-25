97f55dc51de100b190569a97ce846772e56daa12
Vocxtir <rahman.victor@outlook.fr>
2016-02-25 16:57:02 +0100

    Trying to get some text and input displayed on phone

--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
+
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.PixPlat;
 
@@ -14,8 +14,10 @@ import com.mygdx.game.PixPlat;
  * Created by rahman on 25/02/2016.
  */
 public class PlayScreen implements Screen {
-    public Texture texture ;
-    private PixPlat game ;
+    public Texture texture ;    //Images
+    //private Sound //Sound effect caused by an event or smthg else
+    //private Music //Background music that continues
+    private PixPlat game ;      //GameClass or Main
     private OrthographicCamera camera ;
     private Viewport gamePort ;
 
@@ -23,8 +25,10 @@ public class PlayScreen implements Screen {
         this.game = g ;
 
         texture = new Texture("CAVESTORY-001.jpg");
-        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        gamePort = new StretchViewport(800, 480, camera);
+        //camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+        //gamePort = new StretchViewport(800, 480, camera);
 
         camera.update();
     }
@@ -37,14 +41,25 @@ public class PlayScreen implements Screen {
     @Override
     public void render(float delta) {
         clearScreen();
+
+        game.batch.setProjectionMatrix(camera.combined);
+
         game.batch.begin();
         game.batch.draw(this.texture, 150, 150);
+//Some parts to revised
+        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
         game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
     }
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height) ;
     }
 
     @Override
