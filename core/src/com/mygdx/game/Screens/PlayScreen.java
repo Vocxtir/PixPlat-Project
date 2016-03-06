4ae20ad265758b23f0c137951d62f8808dd8db18
Theoo <theoo.ngn@gmail.com>
2016-03-06 17:00:41 +0100

    AJOUT DU 1ER NIVEAU L'Ecran se load sur le premier niveau, laisser appuyer fait défiler le jeu vers la droite

--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -20,6 +23,9 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;//ViewPort permet d'arranger l'affichage selon la taille de l'ecran
     private PixPlat game;
     private HUD hud;
+    private TmxMapLoader mapLoader;//le chargeur de map
+    private TiledMap map;//la map
+    private OrthogonalTiledMapRenderer renderer;
     //Texture texture;
 
     public PlayScreen(PixPlat PP){
@@ -29,8 +35,27 @@ public class PlayScreen implements Screen {
         gameCam=new OrthographicCamera();
         gamePort = new FitViewport(PixPlat.V_WIDTH, PixPlat.V_HEIGHT, gameCam);
         this.hud = new HUD(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("Level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);//Etre sur de commencer au DEBUT DE LA MAP
+        //Et pas au centre X Y O,O
     }
 
+    public void handleInput(float dt){
+        if(Gdx.input.isTouched()){
+            gameCam.position.x += 100 * dt;
+        }
+    }
+
+    //Update le monde
+    public void update(float dt){
+        handleInput(dt);
+        gameCam.update();
+        renderer.setView(gameCam);
+
+    }
     @Override
     public void show() {
 
@@ -38,8 +63,11 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        update(delta);
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);//Clean le screen
+
+        renderer.render();
 
         //Ce qui va etre montré
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
